614d280852ddc3bdbcb87f34e20742d11a6759c3
David Kuo <s50407s@gmail.com>
2018-05-17 02:18:52 +0800

    Deploy at 18/05/17

---
+++ _nuxt/lang-en.716a02d5e109dfc3a361.js
@@ -0,0 +1,1 @@
+webpackJsonp([2],{"J+VR":function(e,s,o){"use strict";Object.defineProperty(s,"__esModule",{value:!0});s.default={pages:{about:"About",schedules:"Schedules",directions:"Direction",sponsors:"Sponsors",staffs:"Staff",blog:"Blog"},register:"Registration"}}});
---
+++ _nuxt/lang-zh-TW.5f4ad6dc36c5ba7df5d4.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{hmmE:function(e,s,t){"use strict";Object.defineProperty(s,"__esModule",{value:!0});s.default={pages:{about:"關於",schedules:"議程",directions:"交通",sponsors:"贊助",staffs:"工作人員",blog:"部落格"},register:"報名"}}});
